5dd868af67ab1543557ff543b04b5e3b23299666
kaseyuu <kaseywu130@gmail.com>
2023-04-10 00:15:27 +1000

    SIT725 P4

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -18,35 +18,60 @@ const clickMe = () => {
 
 const submitForm = () => {
     let formData = {};
-    formData.first_name = $('#first_name').val();
-    formData.last_name = $('#last_name').val();
-    formData.password = $('#password').val();
-    formData.email = $('#email').val();
+    formData.title = $('#title').val();
+    formData.image = $('#image').val();
+    formData.link = $('#link').val();
+    formData.desciption = $('#desciption').val();
 
-    console.log("Form Data Submitted: ", formData);
+    console.log("Form Data: ", formData);
+    addCat(formData);
+}
+
+const addCat = (cat) => {
+    $.ajax({
+        url: 'api/cats',
+        data: cat,
+        type: 'POST',
+        success: (result) => {
+            alert(result.message);
+            location.reload();
+        }
+    });
+}
+
+const getCats = () => {
+    $.get('/api/cats', (response) => {
+        if (response.statusCode === 200) {
+            addCards(response.data);
+        }
+    })
 }
 
 const addCards = (items) => {
     items.forEach(item => {
-        let itemToAppend = '<div class="col s4 center-align">'+
-    '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="'+item.image+'">'+
-    '</div><div class="card-content">'+
-    '<span class="card-title activator grey-text text-darken-4">'+item.title+'<i class="material-icons right">more_vert</i></span><p><a href="#">'+item.link+'</a></p></div>'+
-    '<div class="card-reveal">'+
-        '<span class="card-title grey-text text-darken-4">'+item.title+'<i class="material-icons right">close</i></span>'+
-        '<p class="card-text">'+item.desciption+'</p>'+
-      '</div></div></div>';
-      $("#card-section").append(itemToAppend)
+        let itemToAppend = '<div class="col s4 center-align">' +
+            '<div class="card medium"><div class="card-image waves-effect waves-block waves-light"><img class="activator" src="' + item.image + '">' +
+            '</div><div class="card-content">' +
+            '<span class="card-title activator grey-text text-darken-4">' + item.title + '<i class="material-icons right">more_vert</i></span><p><a href="#">' + item.link + '</a></p></div>' +
+            '<div class="card-reveal">' +
+            '<span class="card-title grey-text text-darken-4">' + item.title + '<i class="material-icons right">close</i></span>' +
+            '<p class="card-text">' + item.desciption + '</p>' +
+            '</div></div></div>';
+        $("#card-section").append(itemToAppend)
     });
 }
 
 
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.materialboxed').materialbox();
-    $('#formSubmit').click(()=>{
+    $('.modal').modal();
+
+    getCats();
+
+    $('#formSubmit').click(() => {
         submitForm();
     })
-    addCards(cardList);
-    $('.modal').modal();
-  });
+
+});
+
